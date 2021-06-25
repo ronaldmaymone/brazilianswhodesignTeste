26780a43d64f9c786f2564218089babac7a559fd
Ronald M <ronaldmaymone@id.uff.br>
2021-06-25 20:49:18 -0300

    vai dar merda

--- pages/api/designers.js
+++ pages/api/designers.js
@@ -1,7 +1,7 @@
 import drive from "drive-db";
 
 export default async (req, res) => {
-  const db = await drive("1RLGOdHcOx1SoayyziY0Z50sWrHRSnGudKzSdkjq7i94");
+  const db = await drive("2PACX-1vTGWvhLSskB3J_Re10p6VPYmF4j8PNE_1FPsOEASlsWT5eBUphI1iNVhMotIsdnjnVZHUX64ViIWg7r");
   let sanitizeResult = db.filter(
     (item) => item.name != "" && item.show == "Yes"
   );
